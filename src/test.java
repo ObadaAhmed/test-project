65d1ff5b0662eafe0b9b40b616b14c91573b02e3
Obada <aboodey33@gmail.com>
2021-07-03 14:10:03 +0200

    First commit

---
+++ src/test.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class test {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int counter = 0;
+        do {
+            System.out.println("hello");
+            counter++;
+        }while (counter < 10);
+    }
+}
